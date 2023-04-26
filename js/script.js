b81eccc0f1ac65e26b05eb5acde51de7f563ee75
Messi-Sui <124379820+Messi-Sui@users.noreply.github.com>
2023-04-26 15:04:54 +0000

    Started

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Copyright (c) 2023 Skelton All rights reserved
+//
+// Created by: Remy Skelton
+// Created on: April 2023
+// This file contains the JS functions for index.html
+"use strict"
+
+function enterClicked () {
+  // create a variable for the movie age limit if statment text
+  let result = "";
+
+  // Use age and day user has entered for results
+  let userAge = parseInt(document.getElementById("user-age").value);
+  let select = document.getElementById('user-day');
+  let day = 
+  
+  
+
+}
